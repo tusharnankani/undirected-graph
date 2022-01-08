62cec5fbb29302296a25bbfcb60e4473175d7899
tusharnankani <tusharnankani3@gmail.com>
2022-01-08 23:27:16 +0530

    :bug: fix: type declaration error for module

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+ declare module '*.css'
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
