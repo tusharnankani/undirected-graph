3014a1608fe1423dff287f941e8c8e0e4a75e506
tusharnankani <tusharnankani3@gmail.com>
2022-01-11 01:44:51 +0530

    :hammer: refactor: increase reader view width

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -92,7 +92,7 @@ export default function PostLayout({
         {`
             .container {
               display: block;
-              max-width: 38rem;
+              max-width: 40rem;
               width: 100%;
               margin: 0 auto;
               padding: 0 1.5rem;
