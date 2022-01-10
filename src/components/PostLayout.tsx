a4a953972daeabab871015c8b9d5828ada166a78
tusharnankani <tusharnankani3@gmail.com>
2022-01-10 16:03:53 +0530

    :hammer: refactor: increase reading width + change highlight

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -92,7 +92,7 @@ export default function PostLayout({
         {`
             .container {
               display: block;
-              max-width: 36rem;
+              max-width: 38rem;
               width: 100%;
               margin: 0 auto;
               padding: 0 1.5rem;
