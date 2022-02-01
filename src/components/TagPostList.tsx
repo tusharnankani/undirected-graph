ebbce51565ba8e734c5b0b163bf849756e2f9233
tusharnankani <tusharnankani3@gmail.com>
2022-02-01 13:14:07 +0530

    :link: link: all posts in `/posts/tag/tag.name`

--- src/components/TagPostList.tsx
+++ src/components/TagPostList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { PostContent } from "../lib/posts";
 import { TagContent } from "../lib/tags";
 import PostItem from "./PostItem";
@@ -16,7 +17,10 @@ export default function TagPostList({ posts, tag, pagination }: Props) {
   return (
     <div className={"container"}>
       <h1>
-        All posts / <span>{tag.name}</span>
+        <Link href="/posts">
+          <a>All posts</a>
+        </Link>{' '}
+        / <span>{tag.name}</span>
       </h1>
       <ul>
         {posts.map((it, i) => (
