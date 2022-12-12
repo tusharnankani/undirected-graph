0c1e92fff25202d67df7d6a02cb18d964eae2562
Tushar Nankani <tusharnankani3@gmail.com>
2022-12-12 19:14:58 +0000

    :sparkles: feat: add website to SocialList

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WebsiteURL from "../assets/web.svg";
 import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
 import config from "../lib/config";
@@ -6,6 +7,14 @@ import config from "../lib/config";
 export function SocialList({}) {
   return (
     <div>
+      <a
+        title="Website"
+        href={config.website_url}
+        target="_blank"
+        rel="noopener"
+      >
+        <WebsiteURL width={24} height={24} fill={"#222"} />
+      </a>
       <a
         title="Twitter"
         href={`https://twitter.com/${config.twitter_account}`}
